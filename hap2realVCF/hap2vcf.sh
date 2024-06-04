98f16eb0d61cc35ed9a3c0e53537850b8a81af9a
qisun2017 <qisun@cornell.edu>
2024-06-04 15:34:51 -0400

    update

--- hap2realVCF/hap2vcf.sh
+++ hap2realVCF/hap2vcf.sh
@@ -6,33 +6,33 @@ numJobs=$3
 
 if [ ! -f $refGenome ]
 then
-	echo "Reference genome fasta file $refGenome not found.\n"
+	echo "Reference genome fasta file $refGenome not found."
 	exit
 fi
 
 if [ ! -f ${refGenome}.bwt ]
 then
-        echo "BWA index for $refGenome not found. There should be a ${refGenome}.bwt file.\n"
+        echo "BWA index for $refGenome not found. There should be a ${refGenome}.bwt file."
 	exit
 fi
 
 if [ ! -d $fastqDir ]
 then
-        echo "Input fastq directory $fastqDir not found.\n"
+        echo "Input fastq directory $fastqDir not found."
         exit
 fi
 
 if [ -n $numJobs ];
 then
-    echo "Parellel jobs number is set to $numJobs.\n"
+    echo "Parellel jobs number is set to $numJobs."
 else
-    echo "Parellel jobs number is not set. Default to 4.\n"
+    echo "Parellel jobs number is not set. Default to 4."
     numJobs=4
 fi 
 
 mkdir ./bam
 
-ls $fastqDir/*fastq | xargs -I {} basename {} | sed "s/.fastq//" | xargs -I {} echo "bwa mem -t 2 -R \"@RG\tID:{}\tSM:{}\tLB:{}\tPL:ILLUMINA\" $refGenome $fastqDir/{}.fastq | samtools sort -O bam -o bam/{}.bam" > tasks_align
+ls $fastqDir | xargs -I {} basename {} | sed "s/.fastq//" | xargs -I {} echo "bwa mem -t 2 -R \"@RG\tID:{}\tSM:{}\tLB:{}\tPL:ILLUMINA\" $refGenome $fastqDir/{}.fastq | samtools sort -O bam -o bam/{}.bam" > tasks_align
 
 parallel -j $numJobs < tasks_align
 
